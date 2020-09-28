1ec7ddab7051eae76a8d3256804e8c4fe1a2cee8
Batuhan Doruk <batuhandoruk91@gmail.com>
2020-09-28 12:45:14 +0300

    Setting the cloned file

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const app = express()
 const bodyParser = require('body-parser')
 
 const cors = require('cors')
+require('dotenv').config();
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose.connection.on('connected', () => {
+  console.log('Database connection is established!')
+});
+
 
 app.use(cors())
 
@@ -41,6 +46,11 @@ app.use((err, req, res, next) => {
   }
   res.status(errCode).type('txt')
     .send(errMessage)
+})
+
+app.post('/api/exercise/new-user', (req,res) => {
+  
+
 })
 
 const listener = app.listen(process.env.PORT || 3000, () => {
